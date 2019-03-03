7146bac2337d4be18087f47bd6ba81f42bee1ce5
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-03-03 12:59:56 +0000

    :sparkles: added feature for users to register If users are not logged in, they can register

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -5,4 +5,6 @@ RSpec.feature "Homepages", type: :feature do
     visit "/"
     expect(page).to have_content("Welcome to Instagram")
   end
+    scenario "Users can Sign up if they are not signed in" do 
+    end
 end
