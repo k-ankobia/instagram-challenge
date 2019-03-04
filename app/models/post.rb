4609d71d5a5d6a13379212b79a39dbfb3b41cfbf
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-03-04 18:04:31 +0000

    :bug: active storage now working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
-    validate :image_presence
-  def image_presence
-    errors.add(:image, "can't be blank") unless image.attached?
-  end
+  #   validate :image_presence
+  # def image_presence
+  #   errors.add(:image, "can't be blank") unless image.attached?
+  # end
   
-  belongs_to :user
+  # belongs_to :user
 
   has_one_attached :image
 end
